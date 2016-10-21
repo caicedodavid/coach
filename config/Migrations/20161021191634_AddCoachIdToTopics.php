cf1b89c63f1f4fa13efb7dffa50c4031def1792e
David <david.caicedo@cleteci.com>
2016-10-21 15:43:09 -0400

    Add coach_id column to Topics

---
+++ config/Migrations/20161021191634_AddCoachIdToTopics.php
@@ -0,0 +1,22 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddCoachIdToTopics extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('topics');
+        $table->addColumn('coach_id', 'char', [
+            'null' => false,
+            'after' => 'duration'
+        ]);
+        $table->update();
+    }
+}
