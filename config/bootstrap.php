00621cce5ff2d150cdc52ef0b3df4daf065235fb
David <david.caicedo@cleteci.com>
2016-12-02 08:58:18 -0400

    changed braincert key

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -196,7 +196,7 @@ Request::addDetector('tablet', function ($request) {
 Configure::write('Users.config', ['users']);
 Configure::write('Session.provider', [
     'name' => 'Braincert',
-    'key' => 'cVipWgpoGG4yOOAREmpB',
+    'key' => 'Dh9rNgeRZCBPWFgFRhqU',
 ]);
 Configure::write('Coach.defaultCommission', 0.25);
 Configure::write('Omnipay.Stripe',[
