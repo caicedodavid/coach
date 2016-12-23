992ac1bc9b3bf59a239a552f3dc9f8f12e4ad2bd
David <david.caicedo@cleteci.com>
2016-12-23 08:35:55 -0400

    set new api key form braincert

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -196,7 +196,7 @@ Request::addDetector('tablet', function ($request) {
 Configure::write('Users.config', ['users']);
 Configure::write('Session.provider', [
     'name' => 'Braincert',
-    'key' => 'BlC4OXwwFORw4UlwQioR',
+    'key' => 'Wn3EW2viQkanYPwUU2Dc',
 ]);
 Configure::write('Coach.defaultCommission', 0.25);
 Configure::write('Omnipay.Stripe',[
