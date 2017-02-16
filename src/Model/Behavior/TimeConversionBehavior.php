c4691aab4e33307432df5f91c3bd02dbc41775fe
David <david.caicedo@cleteci.com>
2017-02-16 12:01:59 -0400

    CLETCOACH-144 Add Time Conversion behavior

---
+++ src/Model/Behavior/TimeConversionBehavior.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Model\Behavior;
+
+use Cake\ORM\Behavior;
+use Cake\Core\Configure;
+use \DateTime;
+use \DateTimeZone;
+
+class TimeConversionBehavior extends Behavior
+{
+
+	/**
+     * Returns Array with Key/Value StatusValue/StatusString for historic view
+     *
+     * @return Array
+     */
+    public function setToUTC($schedule, $timezone) 
+    {
+        $startTime = new DateTime($schedule, new DateTimeZone($timezone));
+        return $startTime->setTimezone(new DateTimeZone(UTC_TIMEZONE))->format('Y-m-d H:i');
+    }        
+      
+}
